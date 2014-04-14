17641b1c558058fdaa2fa782c0d01981eeb3164f
Matthew Seiler <theflyinghyrax@gmail.com>
2014-04-14 13:35:28 -0400

    Added adding new classes (rough)
    Former-commit-id: c770757be73f67f3b624f6f3eae9066a9a0a2a54

--- ruagenda/www/js/index.js
+++ ruagenda/www/js/index.js
@@ -7,45 +7,94 @@ var app = {
     initialize: function() {
         this.bindEvents();
     },
+    
     // Bind Event Listeners
-    //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
+        $( "#new-class-cancel" ).on("click", this.addNewClassCancelHandler);
+        $( "#new-class-save" ).on("click", this.addNewClassSaveHandler);
+        $( "#classList li a#add-new-class-btn" ).on("click", this.addNewClassHandler);
     },
-    // deviceready Event Handler
-    // >> the phonegap api is not ready until this method is called!
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        ldb.initialize();
-        app.initiateClassListUpdate();
+    
+    // initialze the local storage database
+    initializeLDB: function() {
+        html5sql.openDatabase("edu.radford.agenda.db", "RU-Agena-DB", 1024*1024*5);
+        html5sql.process(
+            [
+                "drop table Classes;","drop table Assignments",
+                "create table if not exists Classes ( cname TEXT PRIMARY KEY NOT NULL, ctitle TEXT DEFAULT '' NOT NULL, instructor TEXT, location TEXT, times TEXT );",
+                "insert or ignore into Classes ( cname ) values ( 'none' );",
+                "create table if not exists Assignments ( id INTEGER PRIMARY KEY AUTOINCREMENT, course TEXT NOT NULL REFERENCES Classes(cname) DEFAULT 'none', name TEXT NOT NULL DEFAULT 'Assignment', description TEXT DEFAULT '', dueDate INTEGER, notify INTEGER NOT NULL DEFAULT 0 CHECK(notify == 0 or notify == 1), whenNotify  INTEGER );"
+            ],
+            function() {
+                console.log("Initialized local database tables");
+            },
+            app.logSqlError
+        );
     },
-    /* begins updating the classlist view from the database */
-    initiateClassListUpdate: function() {
-        ldb.getClassList();
-    },
-    /* finishes updating the class list view using objects queried from db
-     * called from websql 'success' callback function
+    
+    /***************************************************************************
+     * Event handlers
+     **************************************************************************/
+
+    /* deviceready Event Handler
+     * >> the phonegap api is not ready until this method is called!
+     * The scope of 'this' is the event. In order to call the 'receivedEvent'
+     * function, we must explicity call 'app.receivedEvent(...);'
      */
-    completeClassListUpdate: function(classList) {
-        $listDom = $( "#classList" );
-        $listDom.empty();
-        $.each(classList, function(i, cls) {
-            $listDom.append( app.generateClassListItem( cls ) );
-        });
-        $listDom.listview('refresh');
-        // bind handlers maybe
-        $("#classList li a").on("click", function() {
-            $("#class-edit").popup("open");
-        });
+    onDeviceReady: function() {
+        app.initializeLDB();
+        app.makeSampleClasses();
     },
-    /* Creates html for a single class section list item using a class section object */
-    generateClassListItem: function(cls) {
-        return "<li><a href='#'>" + cls.name + "<p><strong>" + cls.title + "</strong></p> \
-                <p>" + cls.instructor + "</p><p>" + cls.location + "</p> \
-                <p>" + cls.times +"</p></a></li>";
+    
+    addNewClassHandler: function() {
+        $("#new-class").popup("open");
+    },
+    addNewClassCancelHandler: function() {
+        $("#new-class").popup("close");
+        $("#new-class :text").val("");
+    },
+    addNewClassSaveHandler: function() {
+        var section = new ClassSection( 
+            $("#new-class-id").val(),
+            $("#new-class-title").val(),
+            $("#new-class-who").val(),
+            $("#new-class-where").val(),
+            $("#new-class-when").val()
+        );
+        ClassList.addClass(section);
+        $("#new-class").popup("close");
+        $("#new-class :text").val("");
+    },
+
+    editExistingClassHandler: function() {
+        $("#edit-class").popup("open");
+    },
+     
+    /***************************************************************************
+     * Dev/Debug stuff
+     **************************************************************************/
+    
+    logSqlError: function(error, statement) {
+        console.log(error);
+        console.log(statement);
+    },
+    
+    /* DELETE ME LATER!
+     * Creates some sample class rows to query against.
+     */
+    makeSampleClasses: function() {
+        var someClasses = [
+            new ClassSection("ITEC 110", "Principles of Information Technology", "Dr. Htay", "MG 203", "TR 3:30 - 4:45 PM"),
+            new ClassSection("ITEC 120", "Principles of Computer Science I", "Dr. Braffitt", "DA 225", "MWRF 11 - 11:50 AM"),
+            new ClassSection("MATH 151", "Calculus I", "Cabbage", "RU 314", "MWF 1 - 1:50 PM"),
+            new ClassSection("ART 111", "Art Appreciation", "Pop", "Porterfield", "MWF 9 - 9:50 AM")
+        ];
+        for (var i = 0; i < someClasses.length; ++i) {
+            ClassList.addClass( someClasses[i] );
+        }
     },
 };
 
@@ -56,108 +105,55 @@ var ClassSection = function(_name, _title, _instructor, _location, _times) {
     this.instructor = _instructor;
     this.location = _location;
     this.times = _times;
-}
+};
 ClassSection.prototype.name = '';
 ClassSection.prototype.title = '';
 ClassSection.prototype.instructor = '';
 ClassSection.prototype.location = '';
 ClassSection.prototype.times = '';
 
-/* object literal to encapsulate local database operations */
-var ldb = {
-    
-    /** Initializes the database tables */
-    initialize: function() {
-        html5sql.openDatabase("edu.radford.agenda.db", "RU-Agena-DB", 1024*1024*5);
-        html5sql.process(
-            [
-		"drop table Classes;","drop table Assignments",
-                "create table if not exists Classes ( cname TEXT PRIMARY KEY NOT NULL, ctitle TEXT DEFAULT '' NOT NULL, instructor TEXT, location TEXT, times TEXT );",
-                "insert or ignore into Classes ( cname ) values ( 'none' );",
-                "create table if not exists Assignments ( id INTEGER PRIMARY KEY AUTOINCREMENT, course TEXT NOT NULL REFERENCES Classes(cname) DEFAULT 'none', name TEXT NOT NULL DEFAULT 'Assignment', description TEXT DEFAULT '', dueDate INTEGER, notify INTEGER NOT NULL DEFAULT 0 CHECK(notify == 0 or notify == 1), whenNotify  INTEGER );"
-            ],
-            function() {
-                console.log("Initialized local database tables");
-            },
-            function(error, statement) {
-                console.log(error);
-                console.log(statement);
-            }
-        );
-        this.makeSampleClasses();
-    },
-    
-    /** Queries the database for a list of all classes (i.e., class sections),
-     * and on success creates a list of ClassSection objects and passes the list 
-     * to 'completeClassListUpdate', which generates page elements from the objects
-     */
-    getClassList: function() {
-        var classList = [];
-        var callback = function(transaction, result) {
-            var classList = []
-            if (result != null && result.rows != null) {
-                for (var i = 0; i < result.rows.length; ++i) {
-                    var row = result.rows.item(i);
-                    var cls = new ClassSection(row.cname, 
-                        row.ctitle, row.instructor,
-                        row.location, row.times);
-                    classList.push(cls);
-                }
-            }
-            app.completeClassListUpdate(classList);
-        };
+/* abstraction of list of classes - contains functions to add/edit/remove
+ * list items.  Each function attempts to mutate the local database
+ * and will mutate the DOM if the db dml statement is successful
+ */
+var ClassList = {
+    // add a class specced by a classSection object
+    addClass: function(cls) {
+        // success callback: update the dom if db insert succeeds
+        var addClassToDom = function() {
+            var $newItem = $( ClassList.generateClassListItem(cls) );
+            // for whatever reason, it does not behave w/ insertBefore
+            //$newItem.insertBefore( $("#classList").filter(":last") );
+            $("#classList").append($newItem);
+            $("#classList").listview("refresh");
+            $("#classList").on("click", "li a.class-list-item", app.editExistingClassHandler);
+        }
+        // insert in db
         html5sql.process(
             [{
-                "sql": "Select * from Classes Where cname != 'none';",
-                "data": [],
-                "success": callback
+                "sql": "Insert or Replace into Classes Values (?, ?, ?, ?, ?)",
+                "data": [ cls.name, cls.title, cls.instructor, cls.location, cls.times ],
+                "success": function(transaction, results) {}
             }],
-            function() {
-                console.log("Fetched classes");
-            },
-            function(error, statement) {
-                console.log(error);
-                console.log(statement);
-            }
+            addClassToDom,
+            app.logSqlError
         );
     },
+    // edit: finds the class by id and replaces it with attributes from 
+    // provided classsection object in both db and DOM
+    editClass: function(clsName, newCls) {
+        
+    },
+    // remove a class - deletes from table (don't forget all associated assignments)
+    // and on success removes corresponding list item from the dom
+    removeClass: function(clsName) {
+        
+    },
     
-    /** DELETE ME LATER!
-     * Creates some sample class rows to query against.
-     */
-    makeSampleClasses: function() {
-        var singleSuccess = function(transaction, results) {};
-        html5sql.process(
-            [
-                {
-                    "sql": "Insert or replace into Classes Values (?, ?, ?, ?, ?);",
-                    "data": ["ITEC 110", "Principles of Information Technology", "Dr. Htay", "MG 203", "TR 3:30 - 4:45 PM"],
-                    "success": singleSuccess()
-                },
-                {
-                    "sql": "Insert or replace into Classes Values (?, ?, ?, ?, ?);",
-                    "data": ["ITEC 120", "Principles of Computer Science I", "Dr. Braffitt", "DA 225", "MWRF 11 - 11:50 AM"],
-                    "success": singleSuccess()
-                },
-                {
-                    "sql": "Insert or replace into Classes Values (?, ?, ?, ?, ?);",
-                    "data": ["MATH 151", "Calculus I", "Cabbage", "RU 314", "MWF 1 - 1:50 PM"],
-                    "success": singleSuccess()
-                },
-                {
-                    "sql": "Insert or replace into Classes Values (?, ?, ?, ?, ?);",
-                    "data": ["ART 111", "Art Appreciation", "Pop", "Porterfield", "MWF 9 - 9:50 AM"],
-                    "success": singleSuccess()
-                }                
-            ],
-            function() {
-                console.log("Inserted sample data");
-            },
-            function(error, statement) {
-                console.log(error);
-                console.log(statement);
-            }
-        );
-    }
-    
-}
+    generateClassListItem: function(cls) {
+        return "<li id='" + cls.name + "'><a class='class-list-item' href='#'>\
+                " + cls.name + "<p><strong>" + cls.title + "</strong></p> \
+                <p>" + cls.instructor + "</p><p>" + cls.location + "</p> \
+                <p>" + cls.times +"</p></a></li>";
+    },
+};
