c98cbe1ebaf03a6708323fbc58d3036cac137b5b
Matthew Seiler <theflyinghyrax@gmail.com>
2014-04-15 19:30:40 -0400

    ...a working class list!  Add, edit, remove all (seem) to work
    Former-commit-id: 7e90fc8a772055ff69b7a6e84a63695261fbb7e5

--- ruagenda/www/js/index.js
+++ ruagenda/www/js/index.js
@@ -1,5 +1,4 @@
 /*
- * TODO: Class Edit, Class Delete
  * TODO: migrate database functionality to 'com.phonegap.plugins.sqlite'
  * @see https://github.com/brodysoft/Cordova-SQLitePlugin
  */
@@ -22,23 +21,23 @@
     };
     /* Our primary namespace, bound to 'window' so we can access it from in HTML */
     window.app = {
-        // Application "Constructor"
+        // Entry point.  This function is called from bottom of HTML page.
         initialize: function () {
             this.bindEvents();
         },
-        
-        // Bind Event Listeners
-        // Bind any events that are required on startup. Common events are:
-        // 'load', 'deviceready', 'offline', and 'online'.
+        /* Bind Event Listeners
+         * Bind any events that are required on startup. Common events are:
+         * 'load', 'deviceready', 'offline', and 'online'.
+         * TODO: How to use both jQuery '*ready' events AND the phonegap ready events?
+         */
         bindEvents: function () {
             document.addEventListener('deviceready', this.onDeviceReady, false);
             
-            $("#edit-class-cancel").on("click", this.classPopupCancelHandler);
-            $("#edit-class-save").on("click", this.classPopupSaveHandler);
             $("#edit-class").on("popupafterclose", this.classPopupOnCloseHandler);
+            $("#edit-class-delete").on("click", this.classPopupDeleteHandler);
+            $("#edit-class-save").on("click", this.classPopupSaveHandler);
         },
-        
-        // initialze the local storage database
+        /* Initialize the local storage database */
         initializeLDB: function () {
             html5sql.openDatabase("edu.radford.agenda.db", "RU-Agena-DB", 1024 * 1024 * 5);
             html5sql.process(
@@ -53,47 +52,59 @@
                 app.logSqlError
             );
         },
-        
-        /***************************************************************************
+        /***********************************************************************
          * Event handlers
-         **************************************************************************/
-
+         **********************************************************************/
         /* deviceready Event Handler
-         * >> the phonegap api is not ready until this method is called!
+         * (the phonegap api is not ready until this function is called!)
          * The scope of 'this' is the event. In order to call the 'receivedEvent'
          * function, we must explicity call 'app.receivedEvent(...);'
          */
         onDeviceReady: function () {
             app.initializeLDB();
-            /* Delete this - for initial population if you need more data
+            /* use for initial table population if you need more data
              * app.makeSampleClasses(); */
             app.classList.populateClassList();
         },
-        // buttons that trigger the class detail dialog
+        /* handler when we tapclick the "Add new class" list item */
         addNewClassHandler: function (event) {
+            // hide the delete/remove button
             $("#edit-class-delete").hide();
+            // enable the class title field
             $("#edit-class-id").prop("disabled", false);
+            // set the 'legend' text
+            $("div#edit-class h3").text("Add a new class");
+            // now open the popup
             $("#edit-class").popup("open");
         },
+        /* handler when we tapclick an existing item in the list of classes */
         editClassHandler: function (event) {
+            // show the delete/remove button
             $("#edit-class-delete").show();
+            // disable editing the name/id of the class (it's the key)
             $("#edit-class-id").prop("disabled", true);
+            // set the 'legend' text
+            $("div#edit-class h3").text("Edit class details");
             // need to pre-populate the form w/ values
-            // HOW do we do that?
-            console.log(event.target.id);
-            
-            $("#edit-class-id").val();
-            $("#edit-class-title").val();
-            $("#edit-class-who").val();
-            $("#edit-class-where").val();
-            $("#edit-class-when").val();
-            
+            var lid = event.currentTarget.id;
+            var cls = app.classList.getClassById(lid);
+            $("#edit-class-id").val(cls.name);
+            $("#edit-class-title").val(cls.title);
+            $("#edit-class-who").val(cls.instructor);
+            $("#edit-class-where").val(cls.location);
+            $("#edit-class-when").val(cls.times);
+            // now open the popup
             $("#edit-class").popup("open");
         },
-        // buttons in the class detail popup
-        classPopupCancelHandler: function () {
+        /* handler for the delete button in the class detail view */
+        classPopupDeleteHandler: function () {
+            var cid = $("#edit-class-id").val();
+            if (cid !== "") {
+                app.classList.removeClass(cid);
+            }
             $("#edit-class").popup("close");
         },
+        /* handler for the save button in the class detail view */
         classPopupSaveHandler: function () {
             var section = makeClassSection(
                 $("#edit-class-id").val(),
@@ -102,25 +113,24 @@
                 $("#edit-class-where").val(),
                 $("#edit-class-when").val()
             );
-            app.classList.addClass(section);
+            /* rudimentary opaque validation, no empty class names */
+            if (section.name !== "") {
+                app.classList.saveClass(section);
+            }
             $("#edit-class").popup("close");
         },
-        // clear the text fields in the class detail popup when it closes
+        /* clear the text fields in the class detail popup when it closes */
         classPopupOnCloseHandler: function (event, ui) {
             $("#edit-class :text").val("");
         },
-
-        
-         
         /***************************************************************************
          * Dev/Debug stuff
          **************************************************************************/
-        
+        /* generic and mostly useless SQL error printing callback */
         logSqlError: function (error, statement) {
             console.log(error);
             console.log(statement);
         },
-        
         /* DELETE ME LATER!
          * Creates some sample class rows to query against.
          */
@@ -135,9 +145,10 @@
                 app.classList.addClass(someClasses[i]);
             }
         },
-        
         /***************************************************************************
          * ClassList singleton object
+         * 'Mega-list' object that keeps in-memory class/assignment objects
+         * and has public methods for adding/deleting stuff and things.
          **************************************************************************/
         classList: function () {
             // table of classes, so we don't have to re-query the db all the time
@@ -145,10 +156,11 @@
             
             // generate html to represent a single class in our list of classes
             var getClassListItemHtml = function (cls) {
-                return "<li><a id='" + cls.name + "' class='class-list-item' href='#'>\
-                        " + cls.name + "<p><strong>" + cls.title + "</strong></p> \
-                        <p>" + cls.instructor + "</p><p>" + cls.location + "</p> \
-                        <p>" + cls.times +"</p></a></li>";
+                var piecesArr = ["<li if='", cls.name + "-li", "'><a id='", cls.name,
+                    "' class='class-list-item' href='#'>", cls.name, "<p><strong>",
+                    cls.title, "</strong></p><p>", cls.instructor, "</p><p>", 
+                    cls.location, "</p><p>", cls.times, "</p></a></li>"];
+                return piecesArr.join("");
             };
             
             // rebuild the displayed jQuery listView in the classes tab
@@ -173,7 +185,7 @@
                 /* Given a classSection object, insert to table, add in list, 
                  * and rebuild the view if all that was successful
                  */
-                addClass: function (cls) {
+                saveClass: function (cls) {
                     html5sql.process(
                         [{
                             "sql": "Insert or Replace into Classes Values (?, ?, ?, ?, ?)",
@@ -187,18 +199,22 @@
                         app.logSqlError
                     );
                 },
-                /* Given a class id ("ITEC 324") and a new object to represent
-                 * its attributes, update in the DB and list, and 
-                 * redraw the view if successful
-                 */
-                editClass: function (clsId, newCls) {
-                    // do stuff
-                },
                 /* Given a class id, remove that class from memory and db
                  * and redraw the class list view
                  */
                 removeClass: function (clsId) {
-                    // do stuff
+                    html5sql.process(
+                        [{
+                            "sql": "Delete From Classes Where cname = ?",
+                            "data": [ clsId ],
+                            "success": function (transaction, results) {} // don't care...
+                        }],
+                        function () {
+                            delete allClasses[clsId];
+                            updateClassListDom();
+                        },
+                        app.logSqlError
+                    );
                 },
                 /** queries the database for all class data,
                  * populates allClasses list, and rebuilds the 'view'
@@ -237,6 +253,11 @@
                         }
                     }
                     return idArr;
+                },
+                /* pull one class object from the list by its id
+                 */
+                getClassById: function(cid) {
+                    return allClasses[cid];
                 }
             };  // end public classList methods
         }(),
