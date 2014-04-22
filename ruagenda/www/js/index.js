5e5124f3b670c32cc8f99f558cf48390130dcce5
Matthew Seiler <theflyinghyrax@gmail.com>
2014-04-21 21:18:45 -0400

    Saving a working state in anticipation of much weeping and gnashing of teeth
    Former-commit-id: 6c018cbb6aa9be7d715c3f5b36d44dd0356545c5

--- ruagenda/www/js/index.js
+++ ruagenda/www/js/index.js
@@ -5,29 +5,214 @@
 (function () {
     /* yes please */
     'use strict';
-    /* Structure for a single class/section/course */
-    var makeCourse = function (_name, _title, _instructor, _location, _times) {
+
+    var big;
+
+    /* Structure for a single assginment's attributes */
+    var makeAssignment = function (_title, _desc, _due, _notify) {
         // create a new object from the Object prototype
         var that = Object.create(null);
-        // add class section properties to it
+        // add our custom attributes
+        that.title = _title;
+        that.desc = _desc;
+        that.dueDate = _due;        // these two MUST be Date objects!
+        that.notifyDate = _notify;  // --^
+        // return the extended object
+        return that;
+    };
+
+    /* Structure for a single class/section/course */
+    var makeCourse = function (_name, _title, _instructor, _location, _times) {
+        var that = Object.create(null);
+        // basic properties
         that.name = _name;
         that.title = _title;
         that.instructor = _instructor;
         that.location = _location;
         that.times = _times;
-        // return the extended object
+        // assignments associated with this class
+        that.assignments = [];
+
+        /* some private functions */
+        // compare two assignments to sort by due date
+        function compareTaskDates (t1, t2) {
+            var t1d = t1.dueDate.getTime(),
+                t2d = t1.dueDate.getTime();
+            if (t1d < t2d) {
+                return -1;
+            }
+            else if (t1d > t2d) {
+                return 1;
+            }
+            else {
+                return 0;
+            }
+        }
+        // when we create this object, any existing assignments for it from the db
+        function initializeTaskList () {
+            html5sql.process(
+                [{
+                    "sql": "Select * from Assignments Where course = ?",
+                    "data": [ that.name ],
+                    "success": function (transaction, result) {
+                        var i = 0, rl = result.rows.length, r, someClass;
+                        for (i; i < rl; i += 1) {
+                            r = result.rows.item(i);
+                            someTask = makeAssignment(r.name, r.description, r.dueDate, r.whenNotify);
+                            that.assignments.push(someTask);
+                        }
+                    }
+                }],
+                function () {
+                    // need gui update here
+                },
+                app.logSqlError
+            );
+        }
+        initializeTaskList();
+
+        // add some public methods
+        that.addTask = function (taskObj) {
+            // add to database
+            // on success, add to list
+        };
+        that.editTask = function (taskIndex, newTaskObj) {
+
+        };
+        that.deleteTask = function (taskIndex) {
+
+        };
+        that.deleteAllTasks = function () {
+
+        };
+        that.getTask = function (taskIndex) {
+
+        };
+
         return that;
     };
-    /* Structure for a single assginment's attributes */
-    var makeAssignment = function (_title, _desc, _due, _notify, _course) {
-        var that = Object.create(null);
-        that.title = _title;
-        that.desc = _desc;
-        that.dueDate = _due;
-        that.notifyDate = _notify;
-        that.course = _course;
-        return that;
+    /* this is a constructor, BUT we should only ever call it once.
+      It would be nice if it were a true singleton, but that doesn't really work
+      because we can only instantiate this object in response to the 
+      database being successfully initialized. 
+    */
+    var initCourseList = function () {
+        var allCourses = {};
+        // when we create this singleton, fetch data from the db
+        html5sql.process(
+            [{
+                "sql": "Select * From Classes Where cname != 'none'",
+                "data": [],
+                "success": function (transaction, result) {
+                    var i = 0, rl = result.rows.length, r, someClass;
+                    for (i; i < rl; i += 1) {
+                        r = result.rows.item(i);
+                        someClass = makeCourse(r.cname, r.ctitle, r.instructor, r.location, r.times);
+                        allCourses[someClass.name] = someClass;
+                    }
+                }
+            }],
+            function () {
+                app.domFuncs.updateCourseListDom();
+            },
+            app.logSqlError
+        );
+        // return public methods in an object literal
+        return {
+            /* Given a classSection object, insert to table, add in list, 
+             * and rebuild the view if all that was successful
+             */
+            addCourse: function (course) {
+                html5sql.process(
+                    [{
+                        "sql": "Insert or Replace into Classes Values (?, ?, ?, ?, ?)",
+                        "data": [ course.name, course.title, course.instructor, course.location, course.times ],
+                        "success": function (transaction, results) {} // don't care...
+                    }],
+                    function () {
+                        allCourses[course.name] = course;
+                        app.domFuncs.updateCourseListDom();
+                    },
+                    app.logSqlError
+                );
+            },
+            editCourse: function (courseId, newTitle, newIns, newLoc, newTime) {
+                html5sql.process(
+                    [{
+                        "sql": "Update Table Classes Set ctitle = ?, instructor = ?, location = ?, times = ? Where cname = ?",
+                        "data": [ newTitle, newIns, newLoc, newTime, courseId ],
+                        "success": function (transaction, results) {}
+                    }],
+                    function () {
+                        // update in list
+                        allCourses.courseId.title = newTitle;
+                        allCourses.courseId.instructor = newIns;
+                        allCourses.courseId.location = newLoc;
+                        allCourses.courseId.times = newTime;
+                        // trigger dom refresh
+                        app.domFuncs.updateCourseListDom();
+                    },
+                    app.logSqlError
+                );
+            },
+            /* Given a class id, remove that class from memory and db
+             * and redraw the class list view
+             */
+            deleteCourse: function (courseId) {
+                html5sql.process(
+                    [{
+                        "sql": "Delete From Assignments Where course = ?",
+                        "data": [ courseId ],
+                        "success": function (transaction, results) {}
+                    },
+                    {
+                        "sql": "Delete From Classes Where cname = ?",
+                        "data": [ courseId ],
+                        "success": function (transaction, results) {}
+                    }],
+                    function () {
+                        delete allCourses[courseId];
+                        app.domFuncs.updateCourseListDom();
+                    },
+                    app.logSqlError
+                );
+            },
+            /* Be careful with this!
+             * This will delete every entry in the classes database!
+              */
+            deleteAllCourses: function () {
+                for (var courseId in allCourses) {
+                    if (allCourses.hasOwnProperty(courseId)) {
+                        courseList.deleteOne(courseId);
+                    }
+                }
+            },
+            getCourse: function (courseId) {
+                return allCourses[courseId];
+            },
+            getAllCourses: function () {
+                var res = [];
+                for (var id in allCourses) {
+                    if (allCourses.hasOwnProperty(id)) {
+                        res.push(allCourses[id]);
+                    }
+                }
+                return res;
+            },
+            /* Grab an array of the class id's we are storing in the list
+             */
+            getCourseIds: function () {
+                var id, idArr = [];
+                for (id in allCourses) {
+                    if (allCourses.hasOwnProperty(id)) {
+                        idArr.push(id);
+                    }
+                }
+                return idArr;
+            }
+        };  // end public courseList methods
     };
+
     /* Our primary namespace, bound to 'window' so we can access it from in HTML */
     window.app = {
         // Entry point.  This function is called from bottom of HTML page.
@@ -43,7 +228,6 @@
             document.addEventListener('deviceready', this.onDeviceReady, false);
             $("#edit-class").on("popupafterclose", this.classPopupOnCloseHandler);
             $("#edit-class-delete").on("click", this.classPopupDeleteHandler);
-            $("#edit-class-save").on("click", this.classPopupSaveHandler);
         },
         /* Initialize the local storage database */
         initializeLDB: function () {
@@ -51,11 +235,11 @@
             html5sql.process(
                 [
                     "create table if not exists Classes ( cname TEXT PRIMARY KEY NOT NULL, ctitle TEXT DEFAULT '' NOT NULL, instructor TEXT, location TEXT, times TEXT );",
-                    "insert or ignore into Classes ( cname ) values ( 'none' );",
-                    "create table if not exists Assignments ( id INTEGER PRIMARY KEY AUTOINCREMENT, course TEXT NOT NULL REFERENCES Classes(cname) DEFAULT 'none', name TEXT NOT NULL DEFAULT 'Assignment', description TEXT DEFAULT '', dueDate INTEGER, notify INTEGER NOT NULL DEFAULT 0 CHECK(notify == 0 or notify == 1), whenNotify  INTEGER );"
+                    "create table if not exists Assignments ( id INTEGER PRIMARY KEY AUTOINCREMENT, course TEXT NOT NULL REFERENCES Classes(cname) DEFAULT 'none', name TEXT UNIQUE NOT NULL DEFAULT 'Assignment', description TEXT DEFAULT '', dueDate INTEGER, whenNotify  INTEGER );"
                 ],
                 function () {
                     console.log("Initialized local database tables");
+                    big = initCourseList();
                 },
                 app.logSqlError
             );
@@ -70,18 +254,18 @@
          */
         onDeviceReady: function () {
             app.initializeLDB();
-            /* use for initial table population if you need more data
-             * app.makeSampleClasses(); */
-            app.courseList.readCourseList();
         },
         /* handler when we tapclick the "Add new class" list item */
-        addNewClassHandler: function (/* event */) {
+        addNewClassHandler: function () {
             // hide the delete/remove button
             $("#edit-class-delete").hide();
             // enable the class title field
             $("#edit-class-id").prop("disabled", false);
             // set the 'legend' text
             $("div#edit-class h3").text("Add a new class");
+            // set handler
+            $("#edit-class-save").off("click");
+            $("#edit-class-save").on("click", app.classPopupAddBtnHandler);
             // now open the popup
             $("#edit-class").popup("open");
         },
@@ -96,12 +280,15 @@
             $("div#edit-class h3").text("Edit class details");
             // need to pre-populate the form w/ values
             lid = event.currentTarget.id;
-            cls = app.courseList.getOne(lid);
+            cls = big.getCourse(lid);
             $("#edit-class-id").val(cls.name);
             $("#edit-class-title").val(cls.title);
             $("#edit-class-who").val(cls.instructor);
             $("#edit-class-where").val(cls.location);
             $("#edit-class-when").val(cls.times);
+            // set handler
+            $("#edit-class-save").off("click");
+            $("#edit-class-save").on("click", app.classPopupEditBtnHandler);
             // now open the popup
             $("#edit-class").popup("open");
         },
@@ -109,12 +296,12 @@
         classPopupDeleteHandler: function () {
             var cid = $("#edit-class-id").val();
             if (cid !== "") {
-                app.courseList.deleteOne(cid);
+                big.deleteCourse(cid);
             }
             $("#edit-class").popup("close");
         },
         /* handler for the save button in the class detail view */
-        classPopupSaveHandler: function () {
+        classPopupAddBtnHandler: function () {
             var section = makeCourse(
                 $("#edit-class-id").val(),
                 $("#edit-class-title").val(),
@@ -124,8 +311,18 @@
             );
             /* rudimentary opaque validation, no empty class names */
             if (section.name !== "") {
-                app.courseList.save(section);
+                big.addCourse(section);
             }
+            $("#edit-class").popup("close");
+        },
+        classPopupEditBtnHandler: function () {
+            big.editCourse(
+                $("#edit-class-id").val(),
+                $("#edit-class-title").val(),
+                $("#edit-class-who").val(),
+                $("#edit-class-where").val(),
+                $("#edit-class-when").val()
+            );
             $("#edit-class").popup("close");
         },
         /* clear the text fields in the class detail popup when it closes */
@@ -151,115 +348,10 @@
                 makeCourse("ART 111", "Art Appreciation", "Pop", "Porterfield", "MWF 9 - 9:50 AM")
             ], i = 0, scl = someClasses.length;
             for (i; i < scl; i += 1) {
-                app.courseList.addClass(someClasses[i]);
+                big.addCourse(someClasses[i]);
             }
         },
-        /***************************************************************************
-         * courseList singleton object
-         * 'Mega-list' object that keeps in-memory class/assignment objects
-         * and has public methods for adding/deleting stuff and things.
-         * Note: The private members are comma-separated; all part of the same 'var' declaration
-         **************************************************************************/
-        courseList: (function () {
-            var allCourses = {};
-            // return public methods in an object literal
-            return {
-                /* Given a classSection object, insert to table, add in list, 
-                 * and rebuild the view if all that was successful
-                 */
-                save: function (course) {
-                    html5sql.process(
-                        [{
-                            "sql": "Insert or Replace into Classes Values (?, ?, ?, ?, ?)",
-                            "data": [ course.name, course.title, course.instructor, course.location, course.times ],
-                            "success": function (transaction, results) {} // don't care...
-                        }],
-                        function () {
-                            allCourses[course.name] = course;
-                            app.domFuncs.updateCourseListDom();
-                        },
-                        app.logSqlError
-                    );
-                },
-                /* Given a class id, remove that class from memory and db
-                 * and redraw the class list view
-                 */
-                deleteOne: function (courseId) {
-                    html5sql.process(
-                        [{
-                            "sql": "Delete From Classes Where cname = ?",
-                            "data": [ courseId ],
-                            "success": function (transaction, results) {} // don't care...
-                        }],
-                        function () {
-                            delete allCourses[courseId];
-                            app.domFuncs.updateCourseListDom();
-                        },
-                        app.logSqlError
-                    );
-                },
-                /* Be careful with this!
-                 * This will delete every entry in the classes datatbase!
-                  */
-                deleteAll: function () {
-                    for (var courseId in allCourses) {
-                        if (allCourses.hasOwnProperty(courseId)) {
-                            courseList.deleteOne(courseId);
-                        }
-                    }
-                },
-                getOne: function (courseId) {
-                    // if that id is not in our list, try to fetch it
-                    return allCourses[courseId];
-                },
-                getAll: function () {
-                    var res = [];
-                    for (var id in allCourses) {
-                        if (allCourses.hasOwnProperty(id)) {
-                            res.push(allCourses[id]);
-                        }
-                    }
-                    return res;
-                },
-                /** queries the database for all class data,
-                 * populates allCourses list, and rebuilds the 'view'
-                 */
-                readCourseList: function () {
-                    // clear the allCourses table, jic
-                    allCourses = {};
-                    // call the local db; callbacks re-populate allCourses
-                    html5sql.process(
-                        [{
-                            "sql": "Select * From Classes Where cname != 'none'",
-                            "data": [],
-                            "success": function (transaction, result) {
-                                var i = 0, rl = result.rows.length, r, someClass;
-                                for (i; i < rl; i += 1) {
-                                    r = result.rows.item(i);
-                                    someClass = makeCourse(r.cname, r.ctitle, r.instructor, r.location, r.times);
-                                    allCourses[someClass.name] = someClass;
-                                }
-                            }
-                        }],
-                        function () {
-                            app.domFuncs.updateCourseListDom();
-                        },
-                        app.logSqlError
-                    );
-                },
-                /* Grab an array of the class id's we are storing in the list
-                 */
-                getClassIds: function () {
-                    var id, idArr = [];
-                    for (id in allCourses) {
-                        if (allCourses.hasOwnProperty(id)) {
-                            idArr.push(id);
-                        }
-                    }
-                    return idArr;
-                }
-            };  // end public courseList methods
-        }()),
+        
         /* contains the funcs for updating dom */
         domFuncs: (function () {
             var getCourseListItemHtml = function (cls) {
@@ -273,7 +365,7 @@
                 // rebuild the displayed jQuery listView in the classes tab
                 updateCourseListDom: function () {
                     var id, oneClass, $newBits,
-                        allClasses = app.courseList.getAll(),
+                        allClasses = big.getAllCourses(),
                         $addBtn = $("<li data-icon='plus'><a href='#' id='add-new-class-btn'>Add new class...</a></li>");
                     $("ul#classList").empty();
                     for (id in allClasses) {
